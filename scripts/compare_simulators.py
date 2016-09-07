b29f8a006afd5e0ae7eda29e8755e4398c5d5151
pnbt <chaslot.guillaume@gmail.com>
2016-09-07 10:16:37 -0700

    Improve compare simulator with built in comparison functions, clean cerfa conversion.

--- scripts/compare_simulators.py
+++ scripts/compare_simulators.py
@@ -15,12 +15,16 @@
 """
 
 from lxml import etree
+from input_variable_converter import CerfaOpenFiscaConverter
+from input_variable_converter import CerfaMConverter
+from input_variable_converter import CerfaOnlineComparator
+
 import openfisca_france, random, operator, time, requests
 import argparse
 import sys
 import json
 
-from m_compute import m_compute_from_aliases
+from m_compute import m_compute
 from population_simulator import CerfaPopulationSimulator
 
 """
@@ -35,7 +39,7 @@ PRINTING_MAX_LINES = 30
     Example results of 3 simulations
 """
 
-class TaxbenefitSimulater():
+class TaxbenefitSimulator():
     def __init__(self, testcases):
         self._testcases = testcases
 
@@ -43,7 +47,15 @@ class TaxbenefitSimulater():
         results = []
         result_count = 0
         for test in self._testcases:
-            results.append(self.simulate_one_test(test))
+            result = None
+            while result is None:
+                try:
+                    result = self.simulate_one_test(test)
+                except ArithmeticError as e:
+                    print repr(e)
+                    time.sleep(1)
+                    print 'Error simulating, trying again...'
+            results.append(result)
             result_count += 1
             if verbose:
                 print ('{} simulations done'.format(result_count))
@@ -52,11 +64,11 @@ class TaxbenefitSimulater():
     def simulate_one_test(self, test):
         raise NotImplementedError
 
-
-class OpenfiscaSimulator(TaxbenefitSimulater):
+class OpenfiscaSimulator(TaxbenefitSimulator):
     def __init__(self, testcases):
         self._tax_benefit_system = openfisca_france.FranceTaxBenefitSystem()
         self._testcases = testcases
+        self._converter = CerfaOpenFiscaConverter()
 
     def find_column_by_alias(self, alias):
         columns = self._tax_benefit_system.column_by_name.values()
@@ -75,49 +87,24 @@ class OpenfiscaSimulator(TaxbenefitSimulater):
                        self._tax_benefit_system.column_by_name.get('f' + alias.lower()))
 
     def make_scenario(self, v):
+        openfisca_inputs = self._converter.convert_cerfa_to_openfisca(v, v['year'])
+        print repr(openfisca_inputs)
         return self._tax_benefit_system.new_scenario().init_single_entity(
-            period=v.get('year'),
-            parent1=dict(
-                age=v.get('year', 2014) - v.get('0DA', 1981),
-                salaire_imposable=v.get('1AJ', 0),
-                frais_reels=v.get('1AK', 0),
-                chomeur_longue_duree=v.get('1AI', 0),
-                ppe_tp_sa=v.get('1AX', 0),
-                ppe_du_sa=v.get('1AV', 0),
-                revenu_assimile_pension=v.get('1AP', 0),
-            ),
-            # parent2 = dict(
-            # age = v.get('year', 2014) - v.get('0DB', 1981),
-            # salaire_de_base = v.get('1BJ', 0),
-            # frais_reels = v.get('1BK', 0),
-            # chomeur_longue_duree = v.get('1BI', 0),
-            #     ppe_tp_sa = v.get('1BX', 0),
-            #     ppe_du_sa = v.get('1BV', 0),
-            #     revenu_assimile_pension = v.get('1BP', 0)
-            #     ),
-        )
+            **openfisca_inputs)
 
     def simulate_one_test(self, test):
         scenario = self.make_scenario(test)
         simulation = scenario.new_simulation(trace=True)
 
-        # SIMULATION FOR THE TRACE
-        traceon = 'revdisp'
-        nivvie = simulation.calculate(traceon, '2014', print_trace=False, max_depth=10, show_default_values=False)[0]
-        # trace_explorer = OpenfiscaTraceExplorer(simulation)
-        # trace_explorer.print_trace(traceon, 2)
-        # trace_explorer.save_trace_as_json(traceon, 2)
-
-        # Converting to float because OpenFisca is returning some numpy
+        revdisp = float(simulation.calculate('revdisp', '2014', print_trace=False, max_depth=10, show_default_values=False)[0])
         irpp = float(-simulation.calculate('irpp', '2014')[0])
         credits_impot = float(simulation.calculate('credits_impot', '2014')[0])
         salaire_imposable = float(simulation.calculate('salaire_imposable', '2014')[0])
-        taux_moyen_imposition = float(simulation.calculate('taux_moyen_imposition', '2014')[0])
+        taux_moyen_imposition = float(simulation.calculate('taux_moyen_imposition', '2014')[0]) * 100
         tot_impot = float(simulation.calculate('tot_impot', '2014')[0])
-
-        # Now that we have the 1AJ, we don't need the salaire_brut anymore
+        print 'Irpp openfisca = ' + repr(irpp)
         return {'irpp': irpp, 'credits_impot': credits_impot, 'salaire_imposable': salaire_imposable,
-                'taux_moyen_imposition': taux_moyen_imposition, 'tot_impot': tot_impot}
+                'taux_moyen_imposition': taux_moyen_imposition, 'tot_impot': tot_impot, 'revdisp': revdisp}
 
 class MDescriptions():
     """
@@ -141,39 +128,40 @@ class MDescriptions():
     def get_description(self, variable):
         return self._variable_to_descriptions[variable]
 
-
-class MSimulator(TaxbenefitSimulater):
+class MSimulator(TaxbenefitSimulator):
     def __init__(self, testcases, m_descriptions):
         self._testcases = testcases
         self._m_descriptions = m_descriptions
+        self._converter = CerfaMConverter()
 
     def simulate_one_test(self, test):
         """
             Calls the M code and translate the name of the variable to a human readable name
         """
-        result = m_compute_from_aliases(test)
+        m_inputs = self._converter.convert_cerfa_to_m(test)
+        not_null_inputs = filter(lambda inpu: m_inputs[inpu] > 0, m_inputs)
+
+        print 'M inputs are' + repr(list([ str(input) + ' ' + str(m_inputs[input]) for input in not_null_inputs]))
+        result = m_compute(m_inputs)
+        print 'IRN= ' + repr(int(result['IRN']))
         return {variable: result[variable] for variable in result}
 
-
-class OnlineTaxSimulator(TaxbenefitSimulater):
+class OnlineTaxSimulator(TaxbenefitSimulator):
     def __init__(self, testcases, m_descriptions):
         self._testcases = testcases
         self._m_descriptions = m_descriptions
+        self._comparator = CerfaOnlineComparator()
 
     def simulate_one_test(self, test):
-        saisie_variables = {
-            '0DA': test.get('0DA', 1981),
-            '1AJ': int(test.get('1AJ', -1000000)),
-            'pre_situation_famille': 'C',
-            'pre_situation_residence': 'M',
-            'simplifie': '1',
-            }
-        cgi_url = 'http://www3.finances.gouv.fr/cgi-bin/calc-{}.cgi'.format(test.get('year', 2014))
+        saisie_variables = self._comparator.convert_cerfa_to_online(test)
+        # Year is the year of the tax period
+        print 'Inputs online = ' + repr(saisie_variables)
+        year_calc =  int(test.get('year', 2014)) + 1
+        cgi_url = 'http://www3.finances.gouv.fr/cgi-bin/calc-{}.cgi'.format(year_calc)
         headers = {'User-Agent': 'Calculette-Impots-Python'}
         response = requests.post(cgi_url, headers=headers, data=saisie_variables)
         root_node = etree.fromstring(response.text, etree.HTMLParser())
         return self.iter_results(root_node)
-
 
     def iter_results(self, root_node):
         ignored_input_hidden_names = (
@@ -189,17 +177,18 @@ class OnlineTaxSimulator(TaxbenefitSimulater):
             parent_tag = parent.tag.lower()
             if parent_tag == 'table':
                 tr = parent[parent.index(element) - 1]
-                assert tr.tag.lower() == 'tr', tr
+                # assert tr.tag.lower() == 'tr', tr
             else:
-                assert parent_tag == 'tr', parent_tag
+                # assert parent_tag == 'tr', parent_tag
                 tr = parent
             while True:
-                description = etree.tostring(tr[1]).strip().rstrip(u'*').strip()
+                description = etree.tostring(tr[1]).strip().strip(u'*').strip()
                 if description:
                     break
                 table = tr.getparent()
                 tr = table[table.index(tr) - 1]
-            result[element_name] = float(element.get('value').strip())
+            result[element_name] = float(element.get('value').strip().strip(u'*'))
+        print 'Online IINET= ' + repr(int(result['IINET']))
         return result
 
 class CalculatorComparator():
@@ -220,7 +209,6 @@ class CalculatorComparator():
             return 0
         return min(float(v1) / float(v2), float(v2) / float(v1))
 
-
     def create_empty_results(self, results1, results2):
         empty_results = {}
         for var1 in results1[0]:
@@ -228,7 +216,6 @@ class CalculatorComparator():
             for var2 in results2[0]:
                 empty_results[var1][var2] = 0
         return empty_results
-
 
     def compute_correlations(self, results1, results2):
         assert len(results1) == len(results2)
@@ -295,27 +282,25 @@ class CalculatorComparator():
         print ('Simulating OF')
         timeBeforeOF = time.time()
         of_simulator = OpenfiscaSimulator(test_cases)
-        result = of_simulator.simulate(verbose=True)
+        result = of_simulator.simulate(verbose=False)
         diff = time.time() - timeBeforeOF
         print 'Openfisca took {} seconds'.format(diff)
         return result
-
 
     def simulate_m(self, test_cases, m_descriptions):
         print ('Simulating M')
         timeBeforeM = time.time()
         m_simulator = MSimulator(test_cases, m_descriptions)
-        result = m_simulator.simulate(verbose=True)
+        result = m_simulator.simulate(verbose=False)
         diff = time.time() - timeBeforeM
         print 'M took {} seconds'.format(diff)
         return result
-
 
     def simulate_online(self, test_cases, m_descriptions):
         print ('Simulating Online')
         timeBeforeOL = time.time()
         ol_simulator = OnlineTaxSimulator(test_cases, m_descriptions)
-        result = ol_simulator.simulate(verbose=True)
+        result = ol_simulator.simulate(verbose=False)
         diff = time.time() - timeBeforeOL
         print 'Online took {} seconds'.format(diff)
         return result
@@ -329,31 +314,38 @@ class CalculatorComparator():
             summed_results[key] = summed_results[key] / len(results)
         return summed_results
 
-    def save_as_json(self, name, object):
+    def save_as_json(self, name, objectname):
         with open('../results/' + name, "w") as outfile:
-            json.dump(object, outfile)
+            json.dump(objectname, outfile)
 
-    def load_from_json(self, file):
-        with open( '../results/' + file, 'r') as f:
+    def load_from_json(self, filename):
+        with open('../results/' + filename, 'r') as f:
             return json.load(f)
 
-    def compute_correlations_of_m_online(self, test_cases, only_impot=True, save=None, load=None):
+    def load_results_from_json(self, filename):
+        self.results_openfisca = self.load_from_json(filename + '-openfisca.json')
+        self.results_m = self.load_from_json(filename + '-m.json')
+        self.results_online = self.load_from_json(filename + '-online.json')
+        self.testcases = self.load_from_json(filename + '-testcases.json')
+
+    def compute_correlations_openfisca_m_online(self, test_cases, only_impot=True, save=None, load=None):
         m_descriptions = MDescriptions()
 
         if load:
             results_openfisca = self.load_from_json(load + '-openfisca.json')
             results_m = self.load_from_json(load + '-m.json')
             results_online = self.load_from_json(load + '-online.json')
+            test_cases = (load + '-testcases.json', test_cases)
         else:
+            results_online = self.simulate_online(test_cases, m_descriptions)
             results_openfisca = self.simulate_of(test_cases)
             results_m = self.simulate_m(test_cases, m_descriptions)
-            results_online = self.simulate_online(test_cases, m_descriptions)
 
         if save:
             self.save_as_json(save + '-openfisca.json', results_openfisca)
             self.save_as_json(save + '-m.json', results_m)
             self.save_as_json(save + '-online.json', results_online)
-
+            self.save_as_json(save + '-testcases.json', test_cases)
 
         print ('\n\n ONLINE - M \n')
         correlations, total_amounts = self.compute_correlations(results_online, results_m)
@@ -387,6 +379,36 @@ class CalculatorComparator():
         m = self.simulate_m(test_cases, m_descriptions)
         return m, online
 
+    def get_values_from(self, results, variable_name):
+        return list(result.get(variable_name, 0) for result in results)
+
+    def get_variable_from_m(self, variable_name):
+        return self.get_variable_from_list(variable_name, self.results_m)
+
+    def get_variable_from_online(self, variable_name):
+        return self.get_variable_from_list(variable_name, self.results_online)
+
+    def get_variable_from_openfisca(self, variable_name):
+        return self.get_variable_from_list(variable_name, self.results_openfisca)
+
+    def get_variable_from_testcases(self, variable_name):
+        return self.get_variable_from_list(variable_name, self.testcases)
+
+    def get_variable_from_list(self, variable_name, list_of_results):
+        try:
+            return self.get_values_from(list_of_results, variable_name)
+        except KeyError as e:
+            print 'Warning: key not found. Available keys are: ' + repr(list_of_results[0].keys())
+        except AttributeError:
+            print 'No results loaded. Get some with "compute_correlations_openfisca_m_online" or load some with "load_results_from_json"'
+
+    def get_variable_from_testcases(self, variable_name):
+        try:
+            return self.get_values_from(self.testcases, variable_name)
+        except KeyError as e:
+            print 'Key not found. Available keys are: ' + repr(self.testcases[0].keys())
+
+
     def compare_results(self, test_cases, x_axis, results1, var1, results2, var2):
         r1 = []
         r2 = []
@@ -401,6 +423,14 @@ class CalculatorComparator():
         colors = ['blue'] * len(r1) + ['green'] * len(r2)
         return x, r1, r2, diff, combined, colors
 
+    def plot_comparison(self, x_data, x_variable, y1_data, y1_variable, y2_data, y2_variable):
+        import matplotlib as plt
+        x, r1, r2, diff, combined, colors = self.compare_results(
+            x_data,x_variable,
+            y1_data, y1_variable,
+            y2_data, y2_variable)
+        plt.scatter(x, r1, c=colors)
+
 def main():
     global parser
     parser = argparse.ArgumentParser(description=__doc__)
@@ -408,12 +438,13 @@ def main():
     parser.add_argument('--ir', default=False, type=bool, help='If we only compute the impot sur le revenu')
     parser.add_argument('--save', default=None, type=str, help='Saves the result in a json file')
     parser.add_argument('--load', default=None, type=str, help='Loads from a json file')
+    parser.add_argument('--linear', default=False, type=bool, help='If we generate from a linear distribution instead of gaussian')
 
     args = parser.parse_args()
 
     comparator = CalculatorComparator()
-    test_cases = CerfaPopulationSimulator().generate_test_cases(args.tests)
-    comparator.compute_correlations_of_m_online(test_cases, only_impot=args.ir, save=args.save, load=args.load)
+    test_cases = CerfaPopulationSimulator().generate_test_cases(args.tests, linear=args.linear)
+    comparator.compute_correlations_openfisca_m_online(test_cases, only_impot=args.ir, save=args.save, load=args.load)
 
     return 0
 
